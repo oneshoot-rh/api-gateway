318d8df360d053363b5af4e9bc2df807f8acfbf0
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2024-02-11 15:19:53 +0100

    update: configure spring cloud gateway filters programmaticaly

---
+++ src/main/java/ma/youcode/apigateway/configuration/ExtractTenantIdGatewayFilterFactory.java
@@ -0,0 +1,27 @@
+package ma.youcode.apigateway.configuration;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExtractTenantIdGatewayFilterFactory extends AbstractGatewayFilterFactory<ExtractTenantIdGatewayFilterFactory.Config> {
+
+    public ExtractTenantIdGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            String host = exchange.getRequest().getHeaders().getFirst("Host");
+            String subdomain = host.split("\\.")[0];
+            exchange.getRequest().mutate().header("X-Tenant-Id", subdomain);
+            return chain.filter(exchange);
+        };
+    }
+
+    public static class Config {
+        // You can customize the configuration properties here if needed
+    }
+}
--- src/main/java/ma/youcode/apigateway/configuration/WebClientConfig.java
+++ src/main/java/ma/youcode/apigateway/configuration/WebClientConfig.java
@@ -1,12 +1,20 @@
 package ma.youcode.apigateway.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.filter.factory.StripPrefixGatewayFilterFactory;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebClientConfig {
+
+    private final ExtractTenantIdGatewayFilterFactory filterFactory;
 
     @Bean
     @LoadBalanced
@@ -14,6 +22,19 @@ public class WebClientConfig {
         return WebClient.builder();
     }
 
+
+    @Bean
+    RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("one-shoot-main",routeSpec -> routeSpec
+                        .path("/one-shoot-main/**")
+                        .filters(f -> f
+                                .filter(filterFactory.apply(new ExtractTenantIdGatewayFilterFactory.Config()))
+                                .filter(new StripPrefixGatewayFilterFactory().apply(c -> c.setParts(1))))
+                        .uri("lb://one-shoot-main")
+                ).build();
+
+    }
     // @Bean
     // public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
     // return factory -> factory.configureDefault(
